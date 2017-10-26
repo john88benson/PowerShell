297ed3a8171e8be5acb57b886d13fb0142661250
John Benson <john88benson@gmail.com>
2017-10-26 13:53:43 -0400

    JCB 102617 Sandbox Scripts

---
+++ DemoJunk.ps1
@@ -0,0 +1,16 @@
+﻿$var1 = Read-Host "Movie Name"
+
+$array= @("Blade Runner 2049","John Wick Chapter 2", "Thor: Ragnarok")
+
+foreach ($element in $array) {        
+        if($var1){
+            $speclist = ($element -eq $var1)
+        }
+        else{
+            $speclist = $True
+        }
+        if($speclist -eq $True){
+	        Write-Host "Winner is " $element
+        }
+
+}
